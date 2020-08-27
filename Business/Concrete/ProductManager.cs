8ca9b8716f63b6850a020307268da6fbf2289529
Burak Güldiken <burakguldiken1@gmail.com>
2020-08-27 21:53:36 +0300

    Project architecture

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            //Business Kodları buraya eklenir,misal validation veya eklenen bir ismin tekrar eklenememesi gibi
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        public IResult Delete(Product product)
+        {
+            _productDal.Delete(product);
+            return new SuccessResult(Messages.ProductDeleted);
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+        }
+
+        public IDataResult<List<Product>> GetList()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
+        }
+
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList(p => p.CategoryID == categoryId).ToList());
+        }
+
+        public IResult Update(Product product)
+        {
+            _productDal.Update(product);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+    }
+}
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Başarıyla Eklendi";
+        public static string ProductDeleted = "Ürün Başarıyla Silindi";
+        public static string ProductUpdated = "Ürün Başarıyla Güncellendi";
+
+        public static string UserNotFound = "Kullanıcı Bulunamadı";
+        public static string PasswordError = "Hatalı Şifre";
+        public static string UserAlreadyExists = "Böyle Bir Kullanıcı Zaten Var";
+        public static string UserRegistered = "Kullanıcı Kaydedildi";
+        public static string AccessTokenCreated = "Access Token Başarıyla Oluşturuldu";
+    }
+}
